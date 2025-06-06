d213f6adab04e7ef0299aeb4bb4903bbd5c08bb9
_pyArisu <alicemasase@gmail.com>
2025-06-06 11:20:00 +0200

    added soln two

--- Leetcode/move_zeroes.py
+++ Leetcode/move_zeroes.py
@@ -14,5 +14,23 @@ class Solution:
                 nums.append(num)
         return nums
     
+    def moveZeroes2(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zeroes = []
+        non_zeroes = []
+        for i,v in enumerate(nums):
+            if v == 0:
+                zeroes.append(v)
+            else:
+                non_zeroes.append(v)
+
+        nums.clear()
+        nums.extend(non_zeroes)
+        nums.extend(zeroes)
+
+    
 nums = [0,1,0,3,12]
-print(Solution().moveZeroes(nums))  # Output: [1, 3, 12, 0, 0]  
+Solution().moveZeroes2(nums) 
+print(nums) # Output: [1, 3, 12, 0, 0]  
